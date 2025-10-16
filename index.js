559152d66c819515d4a88c81709cde98c378cf7b
iwhite98 <iwhite98@kaist.ac.kr>
2025-10-17 02:59:22 +0900

    update index.html

---
+++ index.js
@@ -0,0 +1,10 @@
+
+(function(){
+    var s=document.createElement('script');
+    s.src='https://plausible.io/js/pa-Id52nKbFfL-oyYKiCRw7I.js';
+    s.async=true; document.head.appendChild(s);
+    window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)};
+    plausible.init=plausible.init||function(i){plausible.o=i||{}};
+    plausible.init();
+  })();
+  
